de38e3a478c43a29c4b017e631a24b974ea00123
kaysen <liangyongkangg@163.com>
2021-04-18 06:39:43 +0800

    init commit

---
+++ generators/app/index.js
@@ -0,0 +1,45 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+
+    prompting() {
+        return this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your project name',
+                default: this.appname,
+            }
+        ]).then(answers => {
+            this.answers = answers;
+        })
+    }
+
+    writing() {
+
+        // 把每个文件都通过模板转成目标路径
+
+        const templates = [
+            '.editorconfig',
+            '.gitignore',
+            '.prettierignore',
+            '.prettierrc',
+            '.umirc.ts',
+            'package.json',
+            'README.md',
+            'tsconfig.json',
+            'typings.d.ts',
+            'mock/.gitkeep',
+            'src/pages/index.tsx',
+            'src/pages/index.less',
+        ]
+
+        templates.forEach(item => {
+            this.fs.copyTpl(
+                this.templatePath(item),
+                this.destinationPath(item),
+                this.answers);
+        })
+    }
+}
+
